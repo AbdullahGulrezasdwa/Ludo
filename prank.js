62decdbd870996b1b161ca7d51485c1c688a6cdd
AuraFarmingPeople <abdullahgulrez123@gmail.com>
2026-02-08 20:00:26 +0400

    Update prank.js

--- prank.js
+++ prank.js
@@ -1,1 +1,12 @@
+function biasedDiceRoll(player) {
+  let roll = Math.floor(Math.random() * 6) + 1;
 
+  // 🤫 Secret prank
+  if (player.isCodeRed) {
+    if (Math.random() < 0.10 && roll < 6) {
+      roll++; // subtle luck boost
+    }
+  }
+
+  return roll;
+}
